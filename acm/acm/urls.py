1af1742095541cb8acabd1ef07a9bfee4ee943d0
Dagda8 <andrew-erwin@hotmail.com>
2019-07-22 12:15:44 -0700

    Added resources page and configured static folder

--- acm/acm/urls.py
+++ acm/acm/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('resources', TemplateView.as_view(template_name='resources.html'), name='resources'),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
+
